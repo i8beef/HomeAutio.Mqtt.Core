70dbb719d3e1c19e4496d71329a7feaeb57231f0
Michael Hallock <mike@hallock.net>
2018-01-01 16:40:38 -0500

    Resolves #1, migrate to .NET standard 2.0 and refactors a lot of the code to a new MQTT library that is currently maintained

--- src/HomeAutio.Mqtt.Core/ServiceBase.cs
+++ src/HomeAutio.Mqtt.Core/ServiceBase.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using NLog;
-using uPLibrary.Networking.M2Mqtt;
-using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Diagnostics;
 
 namespace HomeAutio.Mqtt.Core
 {
@@ -12,134 +14,168 @@ namespace HomeAutio.Mqtt.Core
     /// </summary>
     public abstract class ServiceBase : IDisposable
     {
-        private ILogger _serviceLog = LogManager.GetCurrentClassLogger();
+        private readonly ILogger<ServiceBase> _serviceLog;
+
+        private readonly string _brokerIp;
+        private readonly int _brokerPort;
+        private readonly string _brokerUsername;
+        private readonly string _brokerPassword;
+
         private bool _disposed = false;
         private bool _stopping;
-
-        private string _brokerUsername;
-        private string _brokerPassword;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ServiceBase"/> class.
         /// </summary>
+        /// <param name="logger">Logging instance.</param>
         /// <param name="brokerIp">MQTT broker IP.</param>
         /// <param name="brokerPort">MQTT broker port.</param>
         /// <param name="brokerUsername">MQTT broker username.</param>
         /// <param name="brokerPassword">MQTT broker password.</param>
         /// <param name="topicRoot">MQTT topic root.</param>
-        public ServiceBase(string brokerIp, int brokerPort, string brokerUsername, string brokerPassword, string topicRoot)
+        public ServiceBase(ILogger<ServiceBase> logger, string brokerIp, int brokerPort, string brokerUsername, string brokerPassword, string topicRoot)
         {
-            SubscribedTopics = new List<string>();
+            _serviceLog = logger;
+            _brokerIp = brokerIp;
+            _brokerPort = brokerPort;
             _brokerUsername = brokerUsername;
             _brokerPassword = brokerPassword;
-
-            // Setup MQTT client
-            if (brokerPort != 1883)
-                MqttClient = new MqttClient(brokerIp, brokerPort, false, MqttSslProtocols.None, null, null);
-            else
-                MqttClient = new MqttClient(brokerIp);
-
-            MqttClientId = Guid.NewGuid();
             TopicRoot = topicRoot;
 
             // Setup mqtt client
-            MqttClient.MqttMsgPublishReceived += Mqtt_MqttMsgPublishReceived;
-
-            // MQTT client logging
-            MqttClient.MqttMsgPublished += (object sender, MqttMsgPublishedEventArgs e) => { _serviceLog.Debug("MQTT message id " + e.MessageId + " sent successfully"); };
-            MqttClient.MqttMsgSubscribed += (object sender, MqttMsgSubscribedEventArgs e) => { _serviceLog.Debug("MQTT subscribe successful with message id " + e.MessageId); };
-            MqttClient.MqttMsgUnsubscribed += (object sender, MqttMsgUnsubscribedEventArgs e) => { _serviceLog.Debug("MQTT unsubscribe successful with message id " + e.MessageId); };
-            MqttClient.ConnectionClosed += (object sender, EventArgs e) =>
-            {
-                if (!_stopping)
-                {
-                    // Unexpected disconnect, restart service
-                    throw new Exception("MQTT Connection closed unexpectedly");
-                }
-                else
-                {
-                    _serviceLog.Info("MQTT Connection closed");
-                }
-            };
-        }
-
-        /// <summary>
-        /// Finalizes an instance of the <see cref="ServiceBase"/> class.
-        /// Override a finalizer only if Dispose(bool disposing) has code to free unmanaged resources.
-        /// </summary>
-        ~ServiceBase()
-        {
-            Dispose(false);
+            SetupMqttLogging();
+            MqttClient.ApplicationMessageReceived += Mqtt_MqttMsgPublishReceived;
         }
 
         /// <summary>
         /// MQTT client.
         /// </summary>
-        protected MqttClient MqttClient { get; private set; }
+        protected IMqttClient MqttClient { get; private set; } = new MqttFactory().CreateMqttClient();
 
         /// <summary>
         /// MQTT client id.
         /// </summary>
-        protected Guid MqttClientId { get; private set; }
+        protected Guid MqttClientId { get; private set; } = Guid.NewGuid();
 
         /// <summary>
         /// Holds list of active MQTT subscriptions.
         /// </summary>
-        protected IList<string> SubscribedTopics { get; private set; }
+        protected IList<string> SubscribedTopics { get; private set; } = new List<string>();
 
         /// <summary>
         /// MQTT Topic root.
         /// </summary>
         protected string TopicRoot { get; private set; }
 
+        /// <summary>
+        /// Setup logging for the MqttClient.
+        /// </summary>
+        private void SetupMqttLogging()
+        {
+            // Log trace messages
+            MqttNetGlobalLogger.LogMessagePublished += (sender, e) =>
+            {
+                switch (e.TraceMessage.Level)
+                {
+                    case MqttNetLogLevel.Error:
+                        _serviceLog.LogError(e.TraceMessage.Message, e.TraceMessage.Exception);
+                        break;
+                    case MqttNetLogLevel.Warning:
+                        _serviceLog.LogWarning(e.TraceMessage.Message);
+                        break;
+                    case MqttNetLogLevel.Info:
+                        _serviceLog.LogInformation(e.TraceMessage.Message);
+                        break;
+                    case MqttNetLogLevel.Verbose:
+                    default:
+                        _serviceLog.LogTrace(e.TraceMessage.Message);
+                        break;
+                }
+            };
+
+            // Log connect and disconnect events
+            MqttClient.Connected += (sender, e) => _serviceLog.LogInformation("MQTT Connection established");
+            MqttClient.Disconnected += (sender, e) =>
+            {
+                if (!_stopping)
+                    throw new Exception("MQTT Connection closed unexpectedly");
+                else
+                    _serviceLog.LogInformation("MQTT Connection closed");
+            };
+        }
+
         #region Service implementation
 
         /// <summary>
         /// Service Start action. Do not call this directly.
         /// </summary>
-        public void Start()
+        /// <returns>Awaitable <see cref="Task" />.</returns>
+        public async Task StartAsync()
         {
-            _serviceLog.Info("Service start initiated");
+            _serviceLog.LogInformation("Service start initiated");
             _stopping = false;
 
-            // Connect to MQTT
+            var optionsBuilder = new MqttClientOptionsBuilder()
+                .WithTcpServer(_brokerIp, _brokerPort)
+                .WithClientId(MqttClientId.ToString())
+                .WithCleanSession();
+
             if (!string.IsNullOrEmpty(_brokerUsername) && !string.IsNullOrEmpty(_brokerPassword))
-                MqttClient.Connect(MqttClientId.ToString(), _brokerUsername, _brokerPassword);
-            else
-                MqttClient.Connect(MqttClientId.ToString());
+                optionsBuilder.WithCredentials(_brokerUsername, _brokerPassword);
+
+            // Connect to MQTT
+            await MqttClient.ConnectAsync(optionsBuilder.Build()).ConfigureAwait(false);
 
             // Subscribe to MQTT messages
-            Subscribe();
+            await SubscribeAsync().ConfigureAwait(false);
 
-            StartService();
+            // Call startup on inheriting service class
+            await StartServiceAsync().ConfigureAwait(false);
 
-            _serviceLog.Info("Service started successfully");
+            _serviceLog.LogInformation("Service started successfully");
         }
 
         /// <summary>
         /// Service Stop action. Do not call this directly.
         /// </summary>
-        public void Stop()
+        /// <returns>Awaitable <see cref="Task" />.</returns>
+        public async Task StopAsync()
         {
-            _serviceLog.Info("Service stop initiated");
+            _serviceLog.LogInformation("Service stop initiated");
 
             _stopping = true;
-            StopService();
+
+            try
+            {
+                // Stop inheriting service class
+                await StopServiceAsync().ConfigureAwait(false);
+
+                // Graceful MQTT disconnect
+                await Unsubscribe().ConfigureAwait(false);
+                await MqttClient.DisconnectAsync().ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                // Log error, but do not rethrow, Dispose should not throw exceptions.
+                _serviceLog.LogError(ex, ex.Message);
+            }
 
             Dispose();
 
-            _serviceLog.Info("Service stopped successfully");
+            _serviceLog.LogInformation("Service stopped successfully");
         }
 
         /// <summary>
         /// HomeAutio service start.
         /// </summary>
-        protected abstract void StartService();
+        /// <returns>Awaitable <see cref="Task" />.</returns>
+        protected abstract Task StartServiceAsync();
 
         /// <summary>
         /// HomeAutio service stop.
         /// </summary>
-        protected abstract void StopService();
+        /// <returns>Awaitable <see cref="Task" />.</returns>
+        protected abstract Task StopServiceAsync();
 
         #endregion
 
@@ -150,40 +186,44 @@ namespace HomeAutio.Mqtt.Core
         /// </summary>
         /// <param name="sender">Event sender.</param>
         /// <param name="e">Event args.</param>
-        protected abstract void Mqtt_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e);
+        protected abstract void Mqtt_MqttMsgPublishReceived(object sender, MqttApplicationMessageReceivedEventArgs e);
 
         /// <summary>
         /// Subscribes to the MQTT topics in SubscribedTopics.
         /// </summary>
-        protected virtual void Subscribe()
+        /// <returns>Awaitable <see cref="Task" />.</returns>
+        protected virtual async Task SubscribeAsync()
         {
             if (MqttClient.IsConnected)
             {
-                _serviceLog.Debug("MQTT subscribing to the following topics: " + string.Join(", ", SubscribedTopics));
-
-                var qosLevels = SubscribedTopics.Select(x => MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE).ToArray();
-                MqttClient.Subscribe(SubscribedTopics.ToArray(), qosLevels);
+                _serviceLog.LogDebug("MQTT subscribing to the following topics: " + string.Join(", ", SubscribedTopics));
+                await MqttClient.SubscribeAsync(SubscribedTopics
+                    .Select(topic => new TopicFilterBuilder()
+                        .WithTopic(topic)
+                        .WithAtLeastOnceQoS()
+                        .Build())).ConfigureAwait(false);
             }
             else
             {
-                _serviceLog.Warn("MQTT could not subscribe to topics on disconnected client");
+                _serviceLog.LogWarning("MQTT could not subscribe to topics on disconnected client");
             }
         }
 
         /// <summary>
         /// Unsubscribes from the MQTT topics in SubscribedTopics.
         /// </summary>
-        protected virtual void Unsubscribe()
+        /// <returns>Awaitable <see cref="Task" />.</returns>
+        protected virtual async Task Unsubscribe()
         {
             // Wipe subscriptions
             if (MqttClient.IsConnected)
             {
-                _serviceLog.Debug("MQTT unsubscribing to the following topics: " + string.Join(", ", SubscribedTopics));
-                MqttClient.Unsubscribe(SubscribedTopics.ToArray());
+                _serviceLog.LogDebug("MQTT unsubscribing to the following topics: " + string.Join(", ", SubscribedTopics));
+                await MqttClient.UnsubscribeAsync(SubscribedTopics).ConfigureAwait(false);
             }
             else
             {
-                _serviceLog.Warn("MQTT could not unsubscribe from topics on disconnected client");
+                _serviceLog.LogWarning("MQTT could not unsubscribe from topics on disconnected client");
             }
         }
 
@@ -203,21 +243,8 @@ namespace HomeAutio.Mqtt.Core
             if (disposing)
             {
                 // Dispose managed state (managed objects).
-            }
-
-            // Free unmanaged resources (unmanaged objects) and override a finalizer below.
-            if (MqttClient != null && MqttClient.IsConnected)
-            {
-                try
-                {
-                    Unsubscribe();
-                    MqttClient.Disconnect();
-                }
-                catch (Exception ex)
-                {
-                    // Log error, but do not rethrow, Dispose should not throw exceptions.
-                    _serviceLog.Error(ex);
-                }
+                if (MqttClient != null)
+                    ((IDisposable)MqttClient).Dispose();
             }
 
             _disposed = true;
@@ -229,7 +256,6 @@ namespace HomeAutio.Mqtt.Core
         public void Dispose()
         {
             Dispose(true);
-            GC.SuppressFinalize(this);
         }
 
         #endregion
